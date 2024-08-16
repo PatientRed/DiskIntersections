b218ec589756af2534b8119a3683696a71106c8f
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-08-16 14:57:07 +0300

    Stubs for calculation methods added

--- DiskIntersections/Program.cs
+++ DiskIntersections/Program.cs
@@ -4,4 +4,17 @@
     {
     }
 
+    public static int DiscIntersections(int[] input)
+    {
+        //can be packed into (input.Length**2)/2 bits structure if you need to consume less memory
+        bool[,] cache = new bool[input.Length, input.Length];
+        int count = 0;
+
+        return count;
+    }
+
+    public static int DiscIntersectionsGoogleEdition(int[] input)
+    {
+        return 0;
+    }
 }
