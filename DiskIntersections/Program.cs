2f8ca99c9d33c60e693b8ce1f87b7f7ee52c8bcf
PatientRed <154473346+PatientRed@users.noreply.github.com>
2024-08-16 14:57:36 +0300

    UpdateIntersections() implemented

--- DiskIntersections/Program.cs
+++ DiskIntersections/Program.cs
@@ -17,4 +17,34 @@
     {
         return 0;
     }
+
+    //O(2*radius-1) === [O(1), O(N)]
+    private static int UpdateIntersections(bool[,] cache, int center, int radius)
+    {
+        int total = 0;
+        int size = cache.GetLength(1);
+
+        //O(2*radius-1)
+        for (int i = center - radius; i <= center + radius; i++)
+        {
+            if (i == center || i < 0 || i >= size)
+                continue;
+
+            if (i < center)
+            {
+                if (!cache[i, center])
+                {
+                    cache[i, center] = true;
+                    total++;
+                }
+            }
+            else if (!cache[center, i])
+            {
+                cache[center, i] = true;
+                total++;
+            }
+        }
+
+        return total;
+    }
 }
